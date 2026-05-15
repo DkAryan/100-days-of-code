0a357a4a1fc33fa1e321734b305edca8b32465d1
DkAryan <dkaryan.codes@gmail.com>
2026-05-15 21:58:41 +0530

    Hey This is my day 15 and project 7 Password Generator is Completed

---
+++ 7_Password-Generator/script.js
@@ -0,0 +1,61 @@
+let display = document.querySelector("#output-display");
+let copyBtn = document.querySelector("#copy-btn");
+
+let upperCase = document.querySelector("#upperCase");
+let lowerCase = document.querySelector("#lowerCase");
+let number = document.querySelector("#number");
+let symbol = document.querySelector("#symbol");
+let generatepass = document.querySelector("#generate-Pass");
+
+let passwordLength = document.querySelector("#passwordLength");
+
+console.log(
+  display,
+  copyBtn,
+  upperCase,
+  lowerCase,
+  number,
+  symbol,
+  passwordLength,
+  generatepass,
+);
+
+generatepass.addEventListener("click", function () {
+  console.log("Button clicked");
+  let pool = "";
+  if (upperCase.checked) {
+    pool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (lowerCase.checked) {
+    pool += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (number.checked) {
+    pool += "0123456789";
+  }
+  if (symbol.checked) {
+    pool += "@#$*&";
+  }
+
+  console.log(pool);
+
+  if (pool === "") {
+    alert("Select at least one option");
+    return;
+  }
+
+  let password = "";
+
+  for (let i = 0; i < passwordLength.value; i++) {
+    let randomnumber = Math.floor(Math.random() * pool.length);
+
+    console.log(randomnumber);
+    let pickedPassword = pool[randomnumber];
+
+    console.log(pickedPassword);
+
+    password += pickedPassword;
+
+    console.log(password);
+  }
+  display.value = password;
+});
